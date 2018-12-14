071900c47b563955d683c21fc61ed679db988a2b
Rodrigo Bello <rodrigoabbello@gmail.com>
2018-12-14 09:42:01 -0200

    Update server recvfrom e sendto

--- server/main.c
+++ server/main.c
@@ -14,15 +14,14 @@
 int create_socket();
 void setup_sockaddr_in(struct sockaddr_in (*), struct sockaddr_in (*));
 void bind_address(struct sockaddr_in (*), int);
-void send_response(int, char (*), int, struct sockaddr_in (*));
-
 
 int main(int argc, char *argv[]) {
 
     int sockfd = create_socket();
     char buffer[MAXLINE];
     char response[MAXLINE];
-    struct sockaddr_in server_address, client_address;
+    struct sockaddr_in server_address = {0};
+    struct sockaddr_in client_address = {0};
 
     setup_sockaddr_in(&server_address, &client_address);
 
@@ -73,7 +72,10 @@ int main(int argc, char *argv[]) {
             break;
     }
 
-    send_response(sockfd, response, len, &client_address);
+    sendto(sockfd, (const char *) response, sizeof(response),
+           MSG_CONFIRM, (const struct sockaddr *) &client_address,
+           len);
+    printf("'%s' sent.\n", response);
 
     return 0;
 }
@@ -104,11 +106,4 @@ void bind_address(struct sockaddr_in *server_address, int sockfd) {
         perror("Fail to bind address");
         exit(EXIT_FAILURE);
     }
-}
-
-void send_response(int sockfd, char* message, int len, struct sockaddr_in *client_address) {
-    sendto(sockfd, (const char *) message, strlen(message),
-           MSG_CONFIRM, (const struct sockaddr *)client_address,
-           len);
-    printf("'%s' sent.\n", message);
 }
